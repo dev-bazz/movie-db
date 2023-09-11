c896325c8945ba1f3cfee7df6a9d29a4bff40dc2
BaZz <bazuaye50@gmail.com>
2023-09-11 05:08:34 +0100

    feat: App routing and data fetching setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
