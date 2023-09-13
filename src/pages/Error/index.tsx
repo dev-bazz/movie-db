8f0006ea49ad2356846b7f985c3fbd22151f0c90
BaZz <bazuaye50@gmail.com>
2023-09-13 18:43:18 +0100

    Error Update

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import style from "./style.module.scss";
 import Lottie from "lottie-react";
 import animation from "./assets/animation_lmhuz9kd.json";
 
 export function Error() {
-	// const error = useRouteError();
+	const error = useRouteError();
+
 	return (
 		<div className={style.container}>
 			<h1>Something Went Wrong</h1>
@@ -12,7 +13,8 @@ export function Error() {
 				<Lottie animationData={animation} />
 			</div>
 
-			{/* <p>{error.statusText || error.message}</p> */}
+			{(error as Error)?.message ||
+				(error as { statusText?: string })?.statusText}
 
 			<Link to="/"> Go to Home</Link>
 		</div>
