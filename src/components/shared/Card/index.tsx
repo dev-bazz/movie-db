d4a580b70512e9b439a6e00a5f6f78c5842c37b6
BaZz <bazuaye50@gmail.com>
2023-09-13 20:31:29 +0100

    heart

--- src/components/shared/Card/index.tsx
+++ src/components/shared/Card/index.tsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
 import { moviesType } from "../../../types/index";
 import style from "./style.module.scss";
+import { useState } from "react";
 export function Card({ movie }: { movie: moviesType }) {
+	const [fav, setFav] = useState(false);
 	return (
 		<article
 			data-testid="movie-card"
 			className={style.card}>
 			<div className="movie-poster">
+				<div
+					onClick={() => setFav(!fav)}
+					className={fav ? `${style.red}` : style.like}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="20"
+						height="21"
+						fill="none"
+						viewBox="0 0 20 21">
+						<path
+							fill={fav ? "#be123c" : "#F9FAFB"}
+							fillRule="evenodd"
+							d="M3.172 5.904c1.562-1.521 4.094-1.521 5.656 0L10 7.044l1.172-1.14c1.562-1.521 4.094-1.521 5.656 0a3.823 3.823 0 010 5.508L10 18.06l-6.828-6.65a3.823 3.823 0 010-5.507z"
+							clipRule="evenodd"></path>
+					</svg>
+				</div>
 				<Link
 					to={{ pathname: `/movie/${movie.id}` }}
 					state={{ movieId: movie.id }}>
