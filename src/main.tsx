a440dd1024be1ce4747949289d72612751f25a6c
BaZz <bazuaye50@gmail.com>
2023-09-13 21:03:49 +0100

    enable refetchon focus

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Error, Movie } from "./pages";
 
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnWindowFocus: false, // default: true
-		},
-	},
-});
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
 	{
 		path: "/",
